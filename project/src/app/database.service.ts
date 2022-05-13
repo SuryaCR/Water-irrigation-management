c1e391eca5f72501e67aef96fb99ca403455b10c
Surya <suryamaverick1404@gmail.com>
2022-05-13 10:43:29 +0530

    commit

--- project/src/app/database.service.ts
+++ project/src/app/database.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class DatabaseService {
   url = 'https://8ca8138b-1aac-430a-8325-3a686242a515-bluemix.cloudantnosqldb.appdomain.cloud/'
   dbUserName = 'apikey-v2-1xzbb618xtgfg14nm7uasm9coajsc9dzzpg8p57atbtg';
   dbPassword = 'f56766c5716a7b37a531aaa7bdb53315';
